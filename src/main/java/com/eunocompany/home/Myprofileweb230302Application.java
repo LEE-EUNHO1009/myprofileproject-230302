a5f342294119f978dbf8aced7c15f2c5bc9debf9
ici <ici@DESKTOP-G48V45S>
2023-03-02 10:03:15 +0900

    기본설정 적용

---
+++ src/main/java/com/eunocompany/home/Myprofileweb230302Application.java
@@ -0,0 +1,13 @@
+package com.eunocompany.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myprofileweb230302Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myprofileweb230302Application.class, args);
+	}
+
+}
